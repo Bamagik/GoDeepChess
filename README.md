e5004018c66b19803f74aa690dfda612adfb4101
Quin Reed <qar4778@rit.edu>
2018-10-14 16:40:24 -0400

    Added README. Board String() function

---
+++ README.md
@@ -0,0 +1,3 @@
+An effort to create a chess engine and an AI that can combat many different situations.
+
+This is my first project in Golang, so I plan to learn a lot along the way in terms of the language.
--- board/board.go
+++ board/board.go
@@ -24,6 +24,19 @@ func main() {
 }
 
 func (board *Board) String() string {
-	str := ""
+	str := "  "
+	for i := 0; i < len(board.board); i++ {
+		str += string(i+97) + " " // get a -> h
+	}
+
+	str += "\n"
+
+	for i := 7; i >= 0; i-- { // i is y axis. i.e 8 -> 1
+		str += string(i+1) + " "
+		for j := 0; j < len(board.board); j++ { // j is the x axis. i.e. a->h
+			str += board.board[i][j].String() + " "
+		}
+	}
+
 	return str
 }
